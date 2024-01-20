2760a999bc7cc96cb28fbb29d25bf851a83aa381
kenandjak <100363820+kenandjak@users.noreply.github.com>
2024-01-19 21:24:54 -0300

    Par e impar - lista
    Submissão correta com auxílio.

--- par_impar_lista.py
+++ par_impar_lista.py
@@ -2,7 +2,13 @@ n = int(input())
 par = []
 impar = []
 for i in range(n):
-    [nome,g] = map(str,input().split(" "))
+    text = input()
+    inputs = list(map(str, text.split(" ")))
+    if (len(inputs) < 2):
+        inputs = map(str, text.split("\t"))
+    
+    [nome, g] = inputs
+        
     if int(g) % 2 == 0:
         par.append(nome)
     else:
@@ -12,30 +18,6 @@ for x in par:
     print(x)
 for y in impar:
     print(y)
-
-'''
-n = int(input())
-par = []
-impar = []
-for i in range(n):
-    nomes = list(map(str,input().split(" ")))
-    if int(nomes[-1]) % 2 == 0:
-        nome = ''
-        for m in range(len(nomes)-1):
-            nome += nomes[m]
-        par.append(nome)
-    else:
-        nome = ''
-        for m in range(len(nomes)-1):
-            nome += nomes[m]
-        impar.append(nome)
-print()
-for x in par:
-    print(x)
-for y in impar:
-    print(y)
-'''
-
 
 '''
 print('ana\t2')'''
