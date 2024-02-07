e7b80c51c9e3708d2877bd5abe73d39fd391ea29
kenandjak <100363820+kenandjak@users.noreply.github.com>
2024-02-07 06:35:36 -0300

    Índice de Poluição

---
+++ indice_poluicao.py
@@ -0,0 +1,10 @@
+i = float(input())
+if i > 0.25 and i<= 0.3:
+    print("Grupo 1")
+elif i <= 0.4:
+    print("Grupo 1")
+    print("Grupo 2")
+elif i > 0.4:
+    print("Grupo 1")
+    print("Grupo 2")
+    print("Grupo 3")
